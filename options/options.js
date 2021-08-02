20d67cc0044624fe28dab8b63ee2e6d97180b28f
Crowdbotics <team@crowdbotics.com>
2021-08-02 23:16:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "quiet_sound_29356Identifier",
+  url: "https://quiet_sound_29356Identifier.botics.co",
+  api: "https://quiet_sound_29356Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
